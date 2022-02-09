8a48e29698100d07c8ed55cefbc89b6c52ef05ac
Keith Crooc <keith.crooc@gmail.com>
2022-02-09 07:20:26 -0500

    base code for Project 28. Changes view dimensions for when keyboard is prompted. Calls in KeychainWrapper allowing us to save a string to it (securely). Also calls on an authentication of FaceID or TouchID when auth button is pressed allowing to see secret message is successful

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -6,14 +6,104 @@
 //
 
 import UIKit
+// call in our biometric authentication stuff
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet var secret: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardDidChangeFrameNotification, object: nil)
+        
+        title = "Nothing to see here"
+        
+//        to watch for when our app is has been "backgrounded" AKA closed, user went to homescreen
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
     }
 
-
+    @IBAction func authenticateTapped(_ sender: Any) {
+//        when we tap the button, call in our authentication tools
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, authenticationError in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+//                        error
+                        let ac = UIAlertController(title: "Authentication Failed", message: "Could not verify, please try again", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+//            no biometry
+            let ac = UIAlertController(title: "Biometry not available", message: "Device is not configured with TouchID or FaceID", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Dismiss", style: .default))
+            self.present(ac, animated: true)
+        }
+        
+        
+        unlockSecretMessage()
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+//        so when we tap screen, our view gets adjusted for when the keyboard is in the way.
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, to: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            secret.contentInset = .zero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
+    
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Write your secrets!"
+        
+//        loading string from keychain is pretty simple.
+//        but you must unwrap it first if there is anything there. (could use nil coalescing)
+        
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+            secret.text = text
+        }
+        
+//        nil coalescing version
+//        secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+        
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+//        resignFirstResponse = tells our view input focus should give up that focus AKA we're done editing and keyboard can go away
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
+    }
+    
 }
 
